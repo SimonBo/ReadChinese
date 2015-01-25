dd3e85a67b064bad04c1bec4f7e93b97747de8eb
Szymon Borucki <szymon.borucki@gmail.com>
2015-01-25 16:44:54 +0800

    Clean up test controller

--- app/controllers/reading_tests_controller.rb
+++ app/controllers/reading_tests_controller.rb
@@ -25,13 +25,12 @@ class ReadingTestsController < ApplicationController
   # POST /reading_tests.json
   def create
     @reading_test = ReadingTest.new(reading_test_params)
+
     test_type = params[:reading_test][:test_type]
     if test_type == 'gap-fill'
       @reading_test.prepare_gap_fill_test
     end
-
-    @reading_test.save
-
+    
     respond_to do |format|
       if @reading_test.save
         format.html { redirect_to @reading_test, notice: 'Reading test was successfully created.' }
